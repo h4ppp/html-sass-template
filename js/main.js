4e79c4b231bb92bfa8dd1d7a87529254eb57355f
unknown <alexey8medvedev@gmail.com>
2022-02-21 07:09:16 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+	//fancybox
+	Fancybox.bind("[data-fancybox]", {
+        infinite: true,
+        dragToClose: false,
+        trapFocus: false,
+        placeFocusBack: false,
+        hideScrollbar: false,
+    });
+
+
+});
